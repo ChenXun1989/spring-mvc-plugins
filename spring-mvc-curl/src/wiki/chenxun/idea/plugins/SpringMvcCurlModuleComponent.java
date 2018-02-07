ca3cd0f68597b02c1d9f5b07bdeafbb628f0a7ac
chenxun <xun.chen@ele.me>
2018-02-07 16:59:18 +0800

    support post params

--- spring-mvc-curl/src/wiki/chenxun/idea/plugins/SpringMvcCurlModuleComponent.java
+++ spring-mvc-curl/src/wiki/chenxun/idea/plugins/SpringMvcCurlModuleComponent.java
@@ -1,15 +1,21 @@
 package wiki.chenxun.idea.plugins;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleComponent;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiModifierListOwner;
+import com.intellij.psi.PsiParameter;
 import com.intellij.psi.javadoc.PsiDocComment;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +23,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 
@@ -31,8 +41,11 @@ public class SpringMvcCurlModuleComponent implements ModuleComponent {
 
     private final Module module;
 
+    private final JavaPsiFacade  javaPsiFacade;
+
     public SpringMvcCurlModuleComponent(Module module) {
         this.module=module;
+        this.javaPsiFacade=JavaPsiFacade.getInstance(module.getProject());
     }
 
     @Override
@@ -124,9 +137,30 @@ public class SpringMvcCurlModuleComponent implements ModuleComponent {
         String httpMethod= requestMapping.findAttributeValue("method").getText().split("\\.")[1];
         PsiDocComment psiDocComment = psiMethod.getDocComment();
         String javaDoc = psiDocComment == null ? "warn no java doc" : psiDocComment.getText();
-        String template="%s \ncurl http://%s:%d%s -X %s \\\n  -H 'Accept-Language: zh-CN,zh;q=0.8' \\\n-H 'Content-type: application/json;charset=UTF-8' \\\n-H 'Accept: */*'  \\\n--compressed";
+        PsiParameter[] psiParameters = psiMethod.getParameterList().getParameters();
+        Map<String, Object> map = new HashMap<>();
+        for(PsiParameter psiParameter:psiParameters){
+            String className = psiParameter.getType().getCanonicalText();
+            String key = psiParameter.getName();
+            GlobalSearchScope searchScope = GlobalSearchScope.everythingScope(module.getProject());
+            if (className.startsWith("java.util.List")) {
+                List list = new ArrayList();
+                className = className.substring(className.indexOf("<") + 1, className.indexOf(">"));
+                Object obj = psiClass2Map(className, searchScope);
+                list.add(obj);
+                map.put(key, list);
+            } else {
+                map.put(key, psiClass2Map(className, searchScope));
+            }
+        }
+        final Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        String param=gson.toJson(map);
 
-        String curl=String.format(template,javaDoc,"localhost",8080,path,httpMethod);
+        String template="%s \ncurl http://%s:%d%s -X %s \\\n  -H 'Accept-Language: zh-CN,zh;q=0.8' \\\n-H 'Content-type: application/json;charset=UTF-8' \\\n-H 'Accept: */*' \\\n--data-binary \\\n%s\\\n--compressed";
+
+        String curl=String.format(template,javaDoc,"localhost",8080,path,httpMethod,param);
         String filePath = psiMethod.getContainingClass().getQualifiedName() + "-" + psiMethod.getName() + ".curl";
 
         File file = new File(baseDir, filePath);
@@ -151,6 +185,30 @@ public class SpringMvcCurlModuleComponent implements ModuleComponent {
 
 
 
+    }
+
+    private Object psiClass2Map(String className, GlobalSearchScope searchScope) {
+        PsiClass psiClass = javaPsiFacade.findClass(className, searchScope);
+        Map<String, Object> map = new HashMap<>();
+        if ("java.lang.Enum".equals(psiClass.getSuperClass().getQualifiedName())) {
+            return "";
+        }
+        PsiField[] fields = psiClass.getFields();
+        if (fields != null) {
+            for (PsiField field : fields) {
+                String fieldClassName = field.getType().getCanonicalText();
+                if (fieldClassName.startsWith("java.util.List")) {
+                    List list = new ArrayList();
+                    fieldClassName = fieldClassName.substring(fieldClassName.indexOf("<") + 1, fieldClassName.indexOf(">"));
+                    Object obj = psiClass2Map(fieldClassName, searchScope);
+                    list.add(obj);
+                    map.put(field.getName(), list);
+                } else {
+                    map.put(field.getName(), psiClass2Map(fieldClassName, searchScope));
+                }
+            }
+        }
+        return map;
     }
 
     private PsiAnnotation getAnnotation(PsiModifierListOwner modifierListOwner , String annotationName) {
